9cd3855addc0c28d733a1f331b4578af4168a76e
jd <duverseaujohnny21@gmail.com>
2020-09-10 14:20:10 -0400

    refactor business logic fetch()

--- src/bike-index.js
+++ src/bike-index.js
@@ -1,40 +1,38 @@
-export default class BikeService {  
-  static getId(zipCode,manufacturer,perPage) {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://bikeindex.org/api/v3/search?page=1&id=${process.env.ID}&secret=${process.env.SECRET}&location=${zipCode}&manufacturer=${manufacturer}&per_page=${perPage}&distance=10&stolenness=stolen`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
-        }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
-  }
-}
-
-// export default class WeatherService {  
-//   static getWeather(city) {
-//     return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_API_KEY}`)
-//       .then(function(response) {
-//         if (!response.ok) {
-//           throw Error(response.statusText);
+// export default class BikeService {  
+//   static getId(zipCode,manufacturer,perPage) {
+//     return new Promise(function(resolve, reject) {
+//       let request = new XMLHttpRequest();
+//       const url = `https://bikeindex.org/api/v3/search?page=1&id=${process.env.ID}&secret=${process.env.SECRET}&location=${zipCode}&manufacturer=${manufacturer}&per_page=${perPage}&distance=10&stolenness=stolen`;
+//       request.onload = function() {
+//         if (this.status === 200) {
+//           resolve(request.response);
+//         } else {
+//           reject(request.response);
 //         }
-//         return response.json();
-//       })
-//       .catch(function(error) {
-//         return Error(error);
-//       })
+//       };
+//       request.open("GET", url, true);
+//       request.send();
+//     });
 //   }
 // }
 
+// triying refactor
 
-
-
-
-
-
-
+export default class BikeService {  
+  static getId(color,zipCode,manufacturer,perPage) {
+    console.log(`getId function running. zipCode: ${zipCode},manufacturer: ${manufacturer},perPage: ${perPage}`)
+    return fetch(`https://bikeindex.org/api/v3/search?page=1&query_items%5B%5D=${color}&id=${process.env.ID}&secret=${process.env.SECRET}&location=${zipCode}&manufacturer=${manufacturer}&per_page=${perPage}&distance=10&stolenness=stolen`)
+      .then(function(bikeResponse) {
+        if (!bikeResponse.ok) {
+          console.log(`Error thrown`);
+          throw Error(bikeResponse.statusText);
+        }
+        console.log(`bikeResponse.Json returned`);
+        return bikeResponse.json();
+      })
+      .catch(function(error) {
+        console.log(`Error caught`);
+        return error;
+      })
+  }
+}
