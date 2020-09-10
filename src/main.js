47626c2f4a5b609af63d198e5479b7646cab1a79
jd <duverseaujohnny21@gmail.com>
2020-09-10 14:18:59 -0400

    refactor it to fetch method
    Co-authored-by: AJ Markow <alexanderjmarkow@gmail.com>
    co-authored-by: Emme Buentiempo <emmettbuentiempo@gmail.com>

--- src/main.js
+++ src/main.js
@@ -4,62 +4,119 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import BikeService from './bike-index.js';
 
+// function displayBikeResults(bikeResponse) { 
+//   const body = JSON.parse(bikeResponse);
+//   body.bikes.forEach(function(element){
+//     $('#bikesfound').append(`
+//     <br>
+//     <div class='col-8 justify-content-center'>
+//     <p class='resultstitle'>🚲 Title: ${element.description}</p>
+//     <p class='results'>Location Stolen: ${element.stolen_location}</p> 
+//     <p class='results'>Date Stolen: ${element.date_stolen}</p>
+//     <p class='results'>Color: ${element.frame_colors[0]}</p> 
+//     <p class='results'>Description: ${element.description}</p>
+//     <p class='results'>Link to page: <A HREF='${element.url}'>${element.url}</A></p>
+//     </div>
+//     </br>`); 
+//   });
+// }
+//   $(document).ready(function() {
+
+// $('#bikeindexsubmit').click(function(event) {
+//     event.preventDefault();
+//     const zipCode = $('#location').val();
+//     const manufacturer = $('#manufacturer').val();
+//     const perPage = $('#results').val();
+
+//     let promise = BikeService.getId(zipCode,manufacturer,perPage);
+//     promise.then(function(bikeResponse) {
+//       console.log('Promise evaluating, not an error state.')
+//       displayBikeResults(bikeResponse);
+//     }, function(error) {
+//       $('.showErrors').text(`There was an error processing your request: ${error}`);
+//     })
+//   })
+// });
+
+
+// trying to refactor
+
 function displayBikeResults(bikeResponse) { 
-  const body = JSON.parse(bikeResponse);
-  body.bikes.forEach(function(element){
+if(bikeResponse.bikes) {
+  bikeResponse.bikes.forEach(function(element){
     $('#bikesfound').append(`
     <br>
     <div class='col-8 justify-content-center'>
     <p class='resultstitle'>🚲 Title: ${element.description}</p>
     <p class='results'>Location Stolen: ${element.stolen_location}</p> 
     <p class='results'>Date Stolen: ${element.date_stolen}</p>
-    <p class='results'>Color: ${element.frame_colors[0]}</p> 
+    <p class='results'>Color: ${element.frame_colors.join(' and ')}</p> 
     <p class='results'>Description: ${element.description}</p>
     <p class='results'>Link to page: <A HREF='${element.url}'>${element.url}</A></p>
     </div>
     </br>`); 
   });
+} else {
+  $('.showErrors').text(`There was an error: ${bikeResponse.message}`);
 }
+}
+
   $(document).ready(function() {
 
 $('#bikeindexsubmit').click(function(event) {
     event.preventDefault();
+    const color = $('#color').val();
     const zipCode = $('#location').val();
-    // const color = $('#colors').val();
     const manufacturer = $('#manufacturer').val();
     const perPage = $('#results').val();
-
-    let promise = BikeService.getId(zipCode,manufacturer,perPage);
-    promise.then(function(bikeResponse) {
-      // if (bikeResponse instanceof Error) {
-      //   throw Error(`OpenWeather API error: ${bikeResponse.message}`);
-      // }
-      console.log('Promise evaluating, not an error state.')
-      displayBikeResults(bikeResponse);
-    }, function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error}`);
-    })
+    // clearFields();
+    BikeService.getId(color,zipCode,manufacturer,perPage)
+      .then(function(bikeResponse) {
+        displayBikeResults(bikeResponse);
+      });
   })
 });
 
-// function clearFields() {
-//   $('#location').val("");
-//   $('.showErrors').text("");
-//   $('.showHumidity').text("");
-//   $('.showTemp').text("");
+
+
+
+// function getElements(response) {
+//   if (response.main) {
+//     $('.showHumidity').text(`The humidity in ${response.name} is ${response.main.humidity}%`);
+//     $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+//   } else {
+//     $('.showErrors').text(`There was an error: ${response.message}`);
+//   }
 // }
 
 // $(document).ready(function() {
 //   $('#weatherLocation').click(function() {
 //     let city = $('#location').val();
 //     clearFields();
-//     let promise = WeatherService.getWeather(city);
-//     promise.then(function(response) {
-//       const body = JSON.parse(response);
-//       $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
-//       $('.showTemp').text(`The temperature in Kelvins is ${body.main.temp} degrees.`);
-//     }, function(error) {
-//       $('.showErrors').text(`There was an error processing your request: ${error}`);
-//     });
+//     WeatherService.getWeather(city)
+//       .then(function(response) {
+//         getElements(response);
+//       });
+//   });
+// });
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function() {
+//   $('#weatherLocation').click(function() {
+//     let city = $('#location').val();
+//     clearFields();
+//     WeatherService.getWeather(city)
+//       .then(function(response) {
+//         getElements(response);
+//       });
 //   });
 // });
